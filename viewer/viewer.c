d27b180623a1dc623fc6ea8ba6cb3896948bfc20
Rodrigo Custodio <jorocuva@gmail.com>
2019-05-31 08:46:40 -0600

    Triangle

--- viewer/viewer.c
+++ viewer/viewer.c
@@ -3,6 +3,7 @@
 #include <assimp/cimport.h>
 #include <assimp/scene.h>
 #include <assimp/postprocess.h>
+#include <GL/glew.h>
 #include <GL/gl.h>
 #include <SDL2/SDL.h>
 #include <stdlib.h>
@@ -16,8 +17,9 @@
 
 static const struct aiScene *scene = NULL;
 static SDL_Window *window = NULL;
-static SDL_GLContext ctx;
+static SDL_Renderer *renderer = NULL;
 static bool running = true;
+static GLuint program;
 
 static float angle = 0.F;
 
@@ -30,6 +32,26 @@ static bool load_model(void)
 		printf("Unable to load model\n");
 		return false;
 	}
+	return true;
+}
+
+static bool read_shader(const char *filename, GLchar **shader, GLint *len)
+{
+	FILE *file = fopen(filename, "rb");
+	if (!file) {
+		printf("Unable to read shader file\n");
+		return false;
+	}
+	fseek(file, 0, SEEK_END);
+	*len = ftell(file);
+	fseek(file, 0, SEEK_SET);
+	*shader = malloc(*len);
+	if (!shader) {
+		printf("Unable to malloc shader string\n");
+		return false;
+	}
+	fread(*shader, 1, *len, file);
+	fclose(file);
 	return true;
 }
 
@@ -54,6 +76,114 @@ static void event_loop(SDL_Event *e)
 	}
 }
 
+static void display(void)
+{
+	/* glClearColor(.5F, .5F, .5F, 1.F); */
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glLoadIdentity();
+	glUseProgram(program);
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+}
+
+void init(void)
+{
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK,
+			    SDL_GL_CONTEXT_PROFILE_CORE);
+
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+	SDL_GL_SetSwapInterval(1);
+
+	GLenum err = glewInit();
+	if (GLEW_OK != err) {
+		printf("Could not load glew %s\n", glewGetErrorString(err));
+		return;
+	}
+
+	GLuint vert_shader = glCreateShader(GL_VERTEX_SHADER);
+	GLuint frag_shader = glCreateShader(GL_FRAGMENT_SHADER);
+	GLchar *vertex_shader_src = NULL;
+	GLchar *fragment_shader_src = NULL;
+	GLint vertex_len = 0;
+	GLint fragment_len = 0;
+	read_shader("shader.vert", &vertex_shader_src, &vertex_len);
+	read_shader("shader.frag", &fragment_shader_src, &fragment_len);
+	if (vertex_shader_src == NULL || fragment_shader_src == NULL) {
+		printf("An error occurred while reading shaders\n");
+		return;
+	}
+
+	const GLchar *vert_src = vertex_shader_src;
+	const GLchar *frag_src = fragment_shader_src;
+	// Susp
+	glShaderSource(vert_shader, 1, &vert_src, NULL);
+	glCompileShader(vert_shader);
+
+	glShaderSource(frag_shader, 1, &frag_src, NULL);
+	glCompileShader(frag_shader);
+
+	GLint is_compiled = 0;
+	glGetShaderiv(vert_shader, GL_COMPILE_STATUS, &is_compiled);
+	if(is_compiled == GL_FALSE)
+	{
+		printf("Error compiling vertex shader\n");
+		glDeleteShader(vert_shader);
+		glDeleteShader(frag_shader);
+		return;
+	}
+	glGetShaderiv(frag_shader, GL_COMPILE_STATUS, &is_compiled);
+	if(is_compiled == GL_FALSE)
+	{
+		printf("Error compiling fragment shader\n");
+		return;
+	}
+
+	program = glCreateProgram();
+	glAttachShader(program, vert_shader);
+	glAttachShader(program, frag_shader);
+	glLinkProgram(program);
+	GLint is_linked = 0;
+	glGetProgramiv(program, GL_LINK_STATUS, &is_linked);
+	if (is_linked == GL_FALSE) {
+		printf("Could not link compiled shaders\n");
+		glDeleteProgram(program);
+		glDeleteShader(vert_shader);
+		glDeleteShader(frag_shader);
+		return;
+	}
+
+	glDetachShader(program, vert_shader);
+	glDetachShader(program, frag_shader);
+
+	GLuint vertex_buffer;
+	GLfloat vertex_data[9] = {
+		-0.5, -0.5, 0.0,
+		0.5, -0.5, 0.0,
+		0.0, 0.5, 0.0,
+	};
+	glGenBuffers(1, &vertex_buffer);
+	glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
+	glBufferData(GL_ARRAY_BUFFER,
+		     sizeof(GLfloat) * 9,
+		     vertex_data,
+		     GL_STATIC_DRAW);
+
+	GLuint vertex_array;
+	glGenVertexArrays(1, &vertex_array);
+	glBindVertexArray(vertex_array);
+	glVertexAttribPointer(
+		0,
+		3,
+		GL_FLOAT,
+		GL_FALSE,
+		4 * 3,
+		(void *) 0);
+	glEnableVertexAttribArray(0);
+	
+
+}
+
 
 bool viewer_start(void)
 {
@@ -68,32 +198,30 @@ bool viewer_start(void)
 				  WIDHT, HEIGHT, SDL_WINDOW_OPENGL);
 	if (window == NULL) {
 		printf("Unable to create window: %s\n", SDL_GetError());
+		SDL_Quit();
 		return false;
 	}
 
-	ctx = SDL_GL_CreateContext(window);
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK,
-			    SDL_GL_CONTEXT_PROFILE_CORE);
+	renderer = SDL_CreateRenderer(window,
+				      -1,
+				      SDL_RENDERER_ACCELERATED |
+				      SDL_RENDERER_PRESENTVSYNC);
+	if (renderer == NULL) {
+		SDL_DestroyWindow(window);
+		printf("Could not create renderer %s\n", SDL_GetError());
+		SDL_Quit();
+		return false;
+	}
 
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
-
-	// Turn on double buffering with a 24bit Z buffer.
-	// You may need to change this to 16 or 32 for your system
-	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-
-
-
-	SDL_GL_SetSwapInterval(1);
+	init();
 	SDL_Event e;
 	while (running) {
 		event_loop(&e);
-		SDL_GL_SwapWindow(window);
-		SDL_Delay(1);
+		display();
+		SDL_RenderPresent(renderer);
+		SDL_Delay(15);
 	}
 
-
-	SDL_GL_DeleteContext(ctx);
 	SDL_Quit();
 }
 
